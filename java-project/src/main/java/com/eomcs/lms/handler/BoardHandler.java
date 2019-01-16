c9e42981152622bb0662970e3291f01517a29c3e
eikhyeonchoi <eikhyeon.choi@gmail.com>
2019-01-16 18:59:04 +0900

    .

--- java-project/src/main/java/com/eomcs/lms/handler/BoardHandler.java
+++ java-project/src/main/java/com/eomcs/lms/handler/BoardHandler.java
@@ -1,46 +1,41 @@
 package com.eomcs.lms.handler;
 
-import java.sql.Date;
 import java.util.Scanner;
 import com.eomcs.lms.domain.Board;
 
 public class BoardHandler {
   
-  final int LENGTH = 10;
-
+  BoardList boardList = new BoardList();
+  // 기본생성자는 배열 10개 생성함
+  // BoardList boardList = new BoardList();
+  // 생성자 파리미터의 int 주면 그만큼 배열생성함
+  
   public Scanner keyboard;
-  Board[] boards = new Board[LENGTH];
-  int boardIdx = 0;
+  Board[] boards;
+  Board b;
   
   public BoardHandler(Scanner key) {
     this.keyboard = key;
   }
 
   public void listBoard() {
-    for (int j = 0; j < this.boardIdx; j++) {
-      System.out.printf("%3d, %-20s, %s, %d\n", 
-          this.boards[j].no, this.boards[j].contents,
-          this.boards[j].createdDate, this.boards[j].viewCount);
+    boards = boardList.toArray();
+    /*
+    for (int j = 0; j < boards.length; j++) {
+      System.out.printf("%3d, %-10s, %s, %d\n", 
+          boards[j].getNo(), boards[j].getContents(),
+          boards[j].getCreatedDate(), boards[j].getViewCount());
+    }
+    */
+    for (Board b : boards) {
+      System.out.printf("%3d, %-10s, %s, %d\n"
+          ,b.getNo(), b.getContents(), b.getCreatedDate(), b.getViewCount());
     }
   }
 
   public void addBoard() {
-    Board board = new Board();
-    
-    System.out.print("번호? ");
-    board.no = Integer.parseInt(this.keyboard.nextLine());
-    
-    System.out.print("내용? ");
-    board.contents = this.keyboard.nextLine();
-    
-    board.createdDate = new Date(System.currentTimeMillis()); 
-    
-    board.viewCount = 0;
-    
-    this.boards[this.boardIdx] = board;
-    this.boardIdx++;
-    
-    System.out.println("저장하였습니다.");
+    boardList.key = keyboard;
+    boardList.add(b);
   }
 
 }
--- java-project/src/main/java/com/eomcs/lms/handler/MemberHandler.java
+++ java-project/src/main/java/com/eomcs/lms/handler/MemberHandler.java
@@ -5,52 +5,27 @@ import java.util.Scanner;
 import com.eomcs.lms.domain.Member;
 
 public class MemberHandler {
-  
-  final int LENGTH = 10;
-  
   public Scanner keyboard;
-  Member[] members = new Member[LENGTH];
-  int memberIdx = 0;
+  MemberList memberList = new MemberList();
+  Member[] members;
+  Member m;
   
   public MemberHandler(Scanner key) {
     this.keyboard = key;
   }
   
   public void listMember() {
-    for (int j = 0; j < this.memberIdx; j++) {
-      System.out.printf("%3d, %-4s, %-20s, %-15s, %s\n", 
-          this.members[j].no, this.members[j].name, this.members[j].email, 
-          this.members[j].tel, this.members[j].registeredDate);
+    members = memberList.toArray();
+    for (Member m : members) {
+      System.out.printf("%3d, %-4s, %-20s, %-15s, %s\n"
+          , m.getNo(), m.getName(), m.getEmail(), 
+          m.getTel(), m.getRegisteredDate());
     }
   }
 
   public void addMember() {
-    Member member = new Member();
-    
-    System.out.print("번호? ");
-    member.no = Integer.parseInt(this.keyboard.nextLine());
-    
-    System.out.print("이름? ");
-    member.name = this.keyboard.nextLine();
-    
-    System.out.print("이메일? ");
-    member.email = this.keyboard.nextLine();
-    
-    System.out.print("암호? ");
-    member.password = this.keyboard.nextLine();
-  
-    System.out.print("사진? ");
-    member.photo = this.keyboard.nextLine();
-  
-    System.out.print("전화? ");
-    member.tel = this.keyboard.nextLine();
-  
-    member.registeredDate = new Date(System.currentTimeMillis()); 
-    
-    this.members[this.memberIdx] = member;
-    this.memberIdx++;
-    
-    System.out.println("저장하였습니다.");
+    memberList.keyboard = keyboard;
+    memberList.add(m);
   }
 
 }
